d8e70a7445a1bf2871bc009ffb079f3b91f7305a
Ashutosh Gupta <ashutoshgupta2737@gmail.com>
2025-09-18 14:30:59 +0530

    added signin controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,73 +1,124 @@
 import User from "../models/UserModel.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
     const { name, email, password, role, institution } = req.body;
 
-    if(!name || !email || !password) {
-        return res.status(400).json({
-            message: "All fields are required"
-        })
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
 
-    if(password.length < 8){
-        return res.status(400).json({
-            message: "Password must be of 8 characters long"
-        })
+    if (password.length < 8) {
+      return res.status(400).json({
+        message: "Password must be of 8 characters long",
+      });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return res.status(400).json({
-            message: "Invalid email format"
-        })
+      return res.status(400).json({
+        message: "Invalid email format",
+      });
     }
 
-    const existingUser = await User.findOne({ email })
-    if(existingUser){
-        return res.status(400).json({
-            message: "User already exists"
-        })
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
 
     const newUser = await User.create({
-        name,
-        email,
-        password,
-        role: role || "student",
-        institution 
-    })
+      name,
+      email,
+      password,
+      role: role || "student",
+      institution,
+    });
 
-    const userToReturn = newUser.toObject()
-    delete userToReturn.password
+    const userToReturn = newUser.toObject();
+    delete userToReturn.password;
 
-    const token = jwt.sign({ userId: newUser._id } ,process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    })
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     res.cookie("jwt", token, {
-        maxAge: 7*24*60*60*1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV == "production"
-    })
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV == "production",
+    });
 
     return res.status(201).json({
-        message: "User created successfully",
+      message: "User created successfully",
+      user: userToReturn,
+      token,
+    });
+  } catch (error) {
+    console.error("Error in signup", error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const userToReturn = user.toObject();
+    delete userToReturn.password;
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      httpOnly: true,
+      secure: process.env.NODE_ENV == "production",
+    });
+
+    return res.status(200).json({
+        message: "Signed in successfully",
         user: userToReturn,
         token
     })
   } catch (error) {
-    console.error("Error in signup", error)
+    console.log("Error in signin controller: ", error)
     return res.status(500).json({
         message: "Internal server error"
     })
   }
 };
 
-export const signin = async (req, res) => {};
-
-export const signout = async (req, res) => {};
+export const signout = async (req, res) => {
+    
+};
 
 export const check = async (req, res) => {};
